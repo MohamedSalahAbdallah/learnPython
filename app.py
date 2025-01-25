e6b85cb8acac97e3e7f6a33715f0d6ae0d509937
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2025-01-25 14:06:22 +0200

    working with strings

--- app.py
+++ app.py
@@ -9,10 +9,30 @@
 
 ########3##########
 
-character_name = "john"
-character_age = 35
-print("there once was a man named " + character_name)
-print("he was "+str(character_age)+" years old")
-character_name='mike'
-print("he really liked the name "+character_name)
-print("but he didn't like being "+str(character_age))
+# character_name = "john"
+# character_age = 35
+# print("there once was a man named " + character_name)
+# print("he was "+str(character_age)+" years old")
+# character_name='mike'
+# print("he really liked the name "+character_name)
+# print("but he didn't like being "+str(character_age))
+
+
+########4##########
+
+
+# phrase = "Python is fun"
+# print(phrase)
+# print(phrase + " yay")
+# print(phrase.lower())
+# print(phrase.upper())
+# print(phrase.capitalize())
+# print(phrase.replace("fun","awesome"))
+# print(len(phrase))
+# print(phrase[0])
+# print(phrase.index("f"))
+# print(phrase.find("f"))
+# print(phrase.count("f"))
+
+
+########5##########
