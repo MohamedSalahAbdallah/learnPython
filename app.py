e2e513593f1999e2a9a2f0e3be78d5750779abef
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2025-01-25 13:50:33 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+########1##########
+# print("first-lesson.py")
+########2##########
+
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
+
+########3##########
+
+print("there once was a man named george")
+print("he was 70 years old")
+print("he really liked the name george")
+print("but he didn't like being 70")
+
